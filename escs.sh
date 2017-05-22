47488eb19bebf292286865aabcb2cabe02aa1595
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2017-05-22 12:55:47 -0300

    First commit.

---
+++ escs.sh
@@ -0,0 +1,10 @@
+##############################################################################
+# push, update.
+
+cd ~/projects/projecteuler.python-code
+git status
+git add *
+git commit -a
+git push
+
+
