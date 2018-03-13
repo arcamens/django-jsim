cf433a71c1655521f3f07e92f78a4ebd0df2c368
Iury de Oliveira Gomes Figueiredo <ioliveira@id.uff.br>
2018-03-13 21:47:32 +0000

    Improving.

---
+++ setup.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name="jsim",
+      version="1.0.0",
+      description="",
+      packages=["jsim"],
+      author="Iury O. G. Figueiredo",
+      author_email="ioliveira@id.uff.br",
+      url='',
+      download_url='',
+      keywords=[],
+      classifiers=[])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
